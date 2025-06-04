3d33a0d010ce15838e391a3e1ab8f8698415a6e4
Yael215128 <yael215128@gmail.com>
2025-06-04 22:05:01 +0300

    change vite.config.ts file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,17 +13,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react'
-            if (id.includes('firebase')) return 'vendor-firebase'
-            return 'vendor' // שאר ספריות צד שלישי
-          }
-        },
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         if (id.includes('react')) return 'vendor-react'
+    //         if (id.includes('firebase')) return 'vendor-firebase'
+    //         return 'vendor' // שאר ספריות צד שלישי
+    //       }
+    //     },
+    //   },
+    // },
     // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
   },
 })
