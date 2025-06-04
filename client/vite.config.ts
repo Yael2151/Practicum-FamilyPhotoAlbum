5bdbcd51113fcf91a1468e542e5809cab58d82c0
Yael215128 <yael215128@gmail.com>
2025-06-04 22:19:54 +0300

    update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,23 +7,52 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             if (id.includes('react')) return 'vendor-react'
+//             if (id.includes('firebase')) return 'vendor-firebase'
+//             return 'vendor' // שאר ספריות צד שלישי
+//           }
+//         },
+//       },
+//     },
+//     // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
+//   },
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    // rollupOptions: {
-    //   output: {
-    //     manualChunks(id) {
-    //       if (id.includes('node_modules')) {
-    //         if (id.includes('react')) return 'vendor-react'
-    //         if (id.includes('firebase')) return 'vendor-firebase'
-    //         return 'vendor' // שאר ספריות צד שלישי
-    //       }
-    //     },
-    //   },
-    // },
-    // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
+    minify: 'terser', // שומר על מיניפיקציה בטוחה
+    terserOptions: {
+      compress: {
+        keep_fnames: true,
+        keep_classnames: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'
+            if (id.includes('firebase')) return 'vendor-firebase'
+            return 'vendor'
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
   },
 })
