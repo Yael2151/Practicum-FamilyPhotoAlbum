2d2cc585e3f1992fd713b3d2eb6cdac00c0eae38
Yael215128 <yael215128@gmail.com>
2025-06-04 23:29:43 +0300

    Reverting the file vite.config to its previous state

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,52 +7,23 @@
 // })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         manualChunks(id) {
-//           if (id.includes('node_modules')) {
-//             if (id.includes('react')) return 'vendor-react'
-//             if (id.includes('firebase')) return 'vendor-firebase'
-//             return 'vendor' // שאר ספריות צד שלישי
-//           }
-//         },
-//       },
-//     },
-//     // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser', // שומר על מיניפיקציה בטוחה
-    terserOptions: {
-      compress: {
-        keep_fnames: true,
-        keep_classnames: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('react')) return 'vendor-react'
             if (id.includes('firebase')) return 'vendor-firebase'
-            return 'vendor'
+            return 'vendor' // שאר ספריות צד שלישי
           }
         },
       },
     },
-    chunkSizeWarningLimit: 1000,
+    // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
   },
 })
