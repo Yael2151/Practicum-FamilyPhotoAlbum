e64fb284fd164202f810d3f4bb599da4995873db
Yael215128 <yael215128@gmail.com>
2025-06-05 19:29:34 +0300

    build client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,32 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react'
-            if (id.includes('firebase')) return 'vendor-firebase'
-            return 'vendor' // שאר ספריות צד שלישי
-          }
-        },
-      },
-    },
-    // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
-  },
+  build:{ 
+    outDir: 'build',
+  }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             if (id.includes('react')) return 'vendor-react'
+//             if (id.includes('firebase')) return 'vendor-firebase'
+//             return 'vendor' // שאר ספריות צד שלישי
+//           }
+//         },
+//       },
+//     },
+//     // chunkSizeWarningLimit: 1000, // העלאת גבול האזהרה ל־1000kB אם אתה רוצה
+//   },
+// })
